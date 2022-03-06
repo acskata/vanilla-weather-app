dc55a4a3525f4edad27dbcccfc748d0df435deb2
acskata <93493050+acskata@users.noreply.github.com>
2022-03-06 22:27:41 +0000

    Added the functional search engine

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
+function search(city) {
+let apiKey = "99b6d7e451166786e8df2d31f57d9b2a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(getTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let inputElement = document.querySelector("#form-input");
+    search(inputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#location-form");
+form.addEventListener("submit", handleSubmit);
+
+
+function getTemperature(response) {
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    let tempElement = document.querySelector("#temperature");
+    tempElement.innerHTML = Math.round(response.data.main.temp);
+    let locationElement = document.querySelector("#location");
+    let city = response.data.name;
+    let country = response.data.sys.country;
+    locationElement.innerHTML = `${city}, ${country}`;
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    let conditionElement = document.querySelector("#condition");
+    conditionElement.innerHTML = response.data.weather[0].description;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+}
+
 function formatDate(timestamp) {
     let date = new Date (timestamp);
     let hours = date.getHours();
@@ -21,30 +58,3 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day}, ${hours}:${minutes}`;
 }
-
-
-
-function getTemperature(response) {
-    let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    let tempElement = document.querySelector("#temperature");
-    tempElement.innerHTML = Math.round(response.data.main.temp);
-    let locationElement = document.querySelector("#location");
-    let city = response.data.name;
-    let country = response.data.sys.country;
-    locationElement.innerHTML = `${city}, ${country}`;
-    let dateElement = document.querySelector("#date");
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    let conditionElement = document.querySelector("#condition");
-    conditionElement.innerHTML = response.data.weather[0].description;
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
-}
-
-let apiKey = "99b6d7e451166786e8df2d31f57d9b2a";
-let place = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(getTemperature);
