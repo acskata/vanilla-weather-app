18da5f26cf032cf13c44cb97ccd44276ff9bbc48
acskata <93493050+acskata@users.noreply.github.com>
2022-03-06 21:08:43 +0000

    Update the date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+function formatDate(timestamp) {
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+      }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ];
+    let day = days[date.getDay()];
+    return `${day}, ${hours}:${minutes}`;
+}
+
+
+
 function getTemperature(response) {
     console.log(response.data);
     let tempElement = document.querySelector("#temperature");
@@ -6,6 +32,8 @@ function getTemperature(response) {
     let city = response.data.name;
     let country = response.data.sys.country;
     cityElement.innerHTML = `${city}, ${country}`;
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     let conditionElement = document.querySelector("#condition");
     conditionElement.innerHTML = response.data.weather[0].description;
     let windElement = document.querySelector("#wind");
@@ -18,31 +46,3 @@ let apiKey = "99b6d7e451166786e8df2d31f57d9b2a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(getTemperature);
-
-let now = new Date();
-
-let date = document.querySelector("#date");
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday"
-];
-let day = days[now.getDay()];
-
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-date.innerHTML = `${day}, ${hours}:${minutes}`;
